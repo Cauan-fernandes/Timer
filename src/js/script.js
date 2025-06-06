de8e83bdd36c705b7f1d4c9f2200c4e056a80a1d
cauan <devcauan@gmail.com>
2025-06-06 16:08:05 -0300

    adicionando toastify

--- src/js/script.js
+++ src/js/script.js
@@ -25,13 +25,40 @@ iniciar.addEventListener('click', function (event) {
   clearInterval(timer)
   startTimer()
   relogio.classList.remove('pause')
+  Toastify({
+    text: "Timer Iniciado",
+    duration: 3000, // duração em ms
+    gravity: "top", // "top" ou "bottom"
+    position: "right", // "left", "center" ou "right"
+    backgroundColor: "#4CAF50", // cor de fundo
+    close: true // botão de fechar
+  }).showToast();
 })
 pause.addEventListener('click', function (event) {
   clearInterval(timer)
   relogio.classList.add('pause')
+  Toastify({
+    text: "Timer Pausado",
+    duration: 3000, // duração em ms
+    gravity: "top", // "top" ou "bottom"
+    position: "right", // "left", "center" ou "right"
+    backgroundColor: "#ffc107", // cor de fundo
+    close: true // botão de fechar
+  }).showToast();
 })
 zerar.addEventListener('click', function (event) {
   clearInterval(timer);
   relogio.innerHTML = '00:00:00'
   segundos = 0
+  Toastify({
+    text: "Timer Zerado",
+    duration: 3000,
+    gravity: "top",
+    position: "right",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    close: true,
+    className: "toast-zerado"
+  }).showToast();
 })
